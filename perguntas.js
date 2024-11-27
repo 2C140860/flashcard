177be0ca1d4af8a3342ad717532708df02fe9c5f
2C140860 <00001103513588sp@al.educacao.sp.gov.br>
2024-11-27 08:42:20 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Animais',
+    'Qual é o maior mamífero do mundo?',
+    'O maior mamífero do mundo é a baleia-azul.'
+)
+/* Sabrina.Rodrigues.2c.26/11/2024
+*/
+criaCartao(
+    'Veterinária',
+    'O que é veterinária?',
+    'Área profissional que se dedica ao cuidado e tratamento de animais.'
+)
+
+criaCartao(
+    'Vôlei',
+    'O que é líbero?',
+    'Um jogador de voleibol especializado em defesa.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz comida?',
+    'Comida em ingles é FOOD (FUDI)'
+)
